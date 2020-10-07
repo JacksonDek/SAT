17ce71ec7c7092ff1b0f3366f60368f31a4ba446
JacksonDek <jackson_dekeyser@outlook.com>
2020-10-07 15:57:56 -0500

    Built Home Controller, Course Controller, Contact Form, and design Login screens

--- SAT.DATA.EF/Metadata/SATMetaData.cs
+++ SAT.DATA.EF/Metadata/SATMetaData.cs
@@ -7,153 +7,163 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SAT.DATA.EF//.Metadata
 {
-    public class SATMetaData
+    public class CourseMetaData
     {
-        public class Course
-        {
-            //public int CouseId { get; set; }
-            [Display(Name = "Course Name" )]
-            [Required(ErrorMessage = "* Required")]
-            [StringLength(50, ErrorMessage = "* Must be 50 characters or less.")]
+        //public int CouseId { get; set; }
+        [Display(Name = "Course Name")]
+        [Required(ErrorMessage = "* Required")]
+        [StringLength(50, ErrorMessage = "* Must be 50 characters or less.")]
 
-            public string CourseName { get; set; }
+        public string CourseName { get; set; }
 
-            [Display(Name = "Course Description")]
-            [Required(ErrorMessage = "* Required")]
-            public string CourseDescription { get; set; }
+        [Display(Name = "Course Description")]
+        [Required(ErrorMessage = "* Required")]
+        public string CourseDescription { get; set; }
 
-            [Display(Name = "Credit Hours")]
-            [Required(ErrorMessage = "* Required")]
-            public byte CreditHours { get; set; }
+        [Display(Name = "Credit Hours")]
+        [Required(ErrorMessage = "* Required")]
+        public byte CreditHours { get; set; }
 
-            [Display(Name = "Curriculum")]
-            [StringLength(250, ErrorMessage = "* Must be 250 characters or less.")]
-            public string Curriculum { get; set; }
+        [Display(Name = "Curriculum")]
+        [StringLength(250, ErrorMessage = "* Must be 250 characters or less.")]
+        public string Curriculum { get; set; }
 
-            [Display(Name = "Notes")]
-            [StringLength(500, ErrorMessage = "* Must be 500 characters or less.")]
-            public string Notes { get; set; }
+        [Display(Name = "Notes")]
+        [StringLength(500, ErrorMessage = "* Must be 500 characters or less.")]
+        public string Notes { get; set; }
 
-            [Display(Name = "Course Status")]
-            [Required(ErrorMessage = "* Required")]
-            public bool IsActive { get; set; }
-        }
-
-        public class Enrollment
-        {
-            //[Required(ErrorMessage = "* Required")]
-            //public int EnrollmentID { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            public int StudentID { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            public int ScheduledClassId { get; set; }
-
-            [Display(Name = "Date Enrolled")]
-            [Required(ErrorMessage = "* Required")]
-            public System.DateTime EnrollmentDate { get; set; }
-        }
-
-        public class ScheduledClass
-        {
-            //public int ScheduledClassId { get; set; }
-            [Required(ErrorMessage = "* Required")]
-            [Display(Name = "Course")]
-            public int CourseId { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            [Display(Name = "Start Date")]
-            public System.DateTime StartDate { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            [Display(Name = "End Date")]
-            public System.DateTime EndDate { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            [StringLength(40, ErrorMessage = "* Must be 40 characters or less.")]
-            [Display(Name = "Instructor")]
-            public string InstructorName { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            [StringLength(20, ErrorMessage = "* Must be 20 characters or less.")]
-            [Display(Name = "Location")]
-            public string Location { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            public int SCSID { get; set; }
-        }
-
-        public class ScheduledClassStatus
-        {
-            //public int SCSID { get; set; }
-            [Required(ErrorMessage = "* Required")]
-            [StringLength(50, ErrorMessage = "* Must be 50 characters or less.")]
-            [Display(Name = "Course Name")]
-            public string SCSName { get; set; }
-        }
-
-        public class Student
-        {
-            [Display(Name = "Student ID")]
-            public int StudentID { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            [StringLength(20, ErrorMessage = "* Must be 20 characters or less.")]
-            [Display(Name = "First Name")]
-            public string FirstName { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            [StringLength(20, ErrorMessage = "* Must be 20 characters or less.")]
-            [Display(Name = "Last Name")]
-            public string LastName { get; set; }
-
-            [Display(Name = "Major")]
-            [StringLength(15, ErrorMessage = "* Must be 15 characters or less.")]
-            public string Major { get; set; }
-
-            [Display(Name = "Address")]
-            [StringLength(50, ErrorMessage = "* Must be 50 characters or less.")]
-            public string Address { get; set; }
-
-            [Display(Name = "City")]
-            [StringLength(25, ErrorMessage = "* Must be 25 characters or less.")]
-            public string City { get; set; }
-
-            [Display(Name = "State")]
-            [StringLength(2, ErrorMessage = "* Must be 2 characters or less.")]
-            public string State { get; set; }
-
-            [Display(Name = "Zip Code")]
-            [StringLength(10, ErrorMessage = "* Must be 10 characters or less.")]
-            public string ZipCode { get; set; }
-
-            [Display(Name = "Phone Number")]
-            [StringLength(13, ErrorMessage = "* Must be 13 characters or less.")]
-            [Phone]
-            public string Phone { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            [Display(Name = "Email Address")]
-            [EmailAddress]
-            public string Email { get; set; }
-
-            public string PhotoUrl { get; set; }
-
-            public int SSID { get; set; }
-        }
-        
-        public class StudentStatus
-        {
-            //public int SSID { get; set; }
-            [Display(Name = "Status")]
-            [StringLength(30, ErrorMessage = "* Must be 30 characters or less.")]
-            public string SSName { get; set; }
-
-            [Required(ErrorMessage = "* Required")]
-            [StringLength(250, ErrorMessage = "* Must be 250 characters or less.")]
-            [Display(Name = "Description")]
-            public string SSDescription { get; set; }
-        }
+        [Display(Name = "Active Course")]
+        [Required(ErrorMessage = "* Required")]
+        public bool IsActive { get; set; }
     }
+    [MetadataType(typeof(CourseMetaData))]
+    public partial class Course { }
+
+    public class EnrollmentMetaData
+    {
+        [Required(ErrorMessage = "* Required")]
+        public int EnrollmentID { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        public int ScheduledClassId { get; set; }
+
+        [Display(Name = "Date Enrolled")]
+        [Required(ErrorMessage = "* Required")]
+        public System.DateTime EnrollmentDate { get; set; }
+    }
+    [MetadataType(typeof(EnrollmentMetaData))]
+    public partial class Enrollment { }
+
+    public class ScheduledClassMetaData
+    {
+        //public int ScheduledClassId { get; set; }
+        [Required(ErrorMessage = "* Required")]
+        [Display(Name = "Course")]
+        public int CourseId { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [Display(Name = "Start Date")]
+        public System.DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [Display(Name = "End Date")]
+        public System.DateTime EndDate { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [StringLength(40, ErrorMessage = "* Must be 40 characters or less.")]
+        [Display(Name = "Instructor")]
+        public string InstructorName { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [StringLength(20, ErrorMessage = "* Must be 20 characters or less.")]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        public int SCSID { get; set; }
+    }
+    [MetadataType(typeof(ScheduledClassMetaData))]
+    public partial class ScheduledClass { }
+
+    public partial class ScheduledClassStatusMetaData
+    {
+        //public int SCSID { get; set; }
+        [Required(ErrorMessage = "* Required")]
+        [StringLength(50, ErrorMessage = "* Must be 50 characters or less.")]
+        [Display(Name = "Course Name")]
+        public string SCSName { get; set; }
+    }
+    [MetadataType(typeof(ScheduledClassStatusMetaData))]
+    public partial class ScheduledClassStatus { }
+
+    public class StudentMetaData
+    {
+        [Display(Name = "Student ID")]
+        public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [StringLength(20, ErrorMessage = "* Must be 20 characters or less.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [StringLength(20, ErrorMessage = "* Must be 20 characters or less.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Major")]
+        [StringLength(15, ErrorMessage = "* Must be 15 characters or less.")]
+        public string Major { get; set; }
+
+        [Display(Name = "Address")]
+        [StringLength(50, ErrorMessage = "* Must be 50 characters or less.")]
+        public string Address { get; set; }
+
+        [Display(Name = "City")]
+        [StringLength(25, ErrorMessage = "* Must be 25 characters or less.")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        [StringLength(2, ErrorMessage = "* Must be 2 characters or less.")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        [StringLength(10, ErrorMessage = "* Must be 10 characters or less.")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [StringLength(13, ErrorMessage = "* Must be 13 characters or less.")]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [Display(Name = "Email Address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public int SSID { get; set; }
+    }
+    [MetadataType(typeof(StudentMetaData))]
+    public partial class Student { }
+
+    public class StudentStatusMetaData
+    {
+        //public int SSID { get; set; }
+        [Display(Name = "Status")]
+        [StringLength(30, ErrorMessage = "* Must be 30 characters or less.")]
+        public string SSName { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [StringLength(250, ErrorMessage = "* Must be 250 characters or less.")]
+        [Display(Name = "Description")]
+        public string SSDescription { get; set; }
+    }
+    [MetadataType(typeof(StudentStatusMetaData))]
+    public partial class StudentStatus { }
+
 }
--- SAT.UI.MVC/Controllers/HomeController.cs
+++ SAT.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Web.Mvc;
+using SAT.UI.MVC.Models;
+using System.Net.Mail; // mail
+using System.Net; // mail
+using System; // built in esception handling
 
 namespace SAT.UI.MVC.Controllers
 {
@@ -11,20 +15,61 @@ namespace SAT.UI.MVC.Controllers
         }
 
         [HttpGet]
-        [Authorize]
-        public ActionResult About()
+        public ActionResult Contact()
         {
-            ViewBag.Message = "Your app description page.";
-
             return View();
         }
 
-        [HttpGet]
-        public ActionResult Contact()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
         {
-            ViewBag.Message = "Your contact page.";
+            // Check that the cvm data just passed into the method is a calid ContactViewModel
+            if (ModelState.IsValid)
+            {
+                // Build the message
 
-            return View();
+                string message = $"You have recieved an email from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br/>{cvm.Message}";
+
+                MailMessage mm = new MailMessage("admin@yoursite.com", "admin@yoursite.com", cvm.Subject, message);
+                // MailMessage Properties
+                // Allow HTML formatting in the Email
+                mm.IsBodyHtml = true;
+                mm.Priority = MailPriority.High;
+                // Respond to the sender and not the admin@yoursiteurl.com
+                mm.ReplyToList.Add(cvm.Email);
+                mm.CC.Add("yourCCperson@yoursite.com");
+
+                // SmtpClient - This is the infor from the host that allows this message to be sent
+                SmtpClient client = new SmtpClient("mail.yourmaildomain.com");
+
+                // Client Credentials
+                client.Credentials = new NetworkCredential("admin@yoursite.com", "Your Password"); // Clear test password is NOT best practice
+
+                // Set the SMTP port if needed
+                client.Port = 8889;
+
+                // Try to send the email
+                try
+                {
+                    // Attempt to send 
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = $"We're sorry your request could not be completed at the time. Please try again later. Error Message: <br/> {ex.StackTrace}";
+                    return View(cvm);
+                }
+
+                // Everything has been sent, so send the user to a confirmation page that is also strongly typed 
+                return View("EmailConfirmation", cvm);
+            }
+            else // The ModelState is NOT valid
+            {
+                // Send back the form with the completed inputs so the user can try again and not have to re-type everything
+                return View(cvm);
+            }
+
         }
     }
 }
